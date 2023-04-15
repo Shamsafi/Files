7fd5160eb1958652624c0bc4a127b0b6d1bff4e8
Safi <mdsafi729@gmail.com>
2023-04-15 14:41:53 +0530

    add file

---
+++ UseCase1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UseCase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                // Define the two points that define the line
+                double x1 = 1;
+                double y1 = 2;
+                double x2 = 4;
+                double y2 = 6;
+
+                // Calculate the length of the line
+                double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+                Console.WriteLine($"The length of the line is {length}");
+            
+        }
+    }
+}
